8c853ba3769b22417b2120ce62fc0602b359cb82
Rourkeyboy <rourkeyboy@gmail.com>
2021-02-05 19:46:22 -0700

    Block chain tutorial from c-sharp corner
    https://www.c-sharpcorner.com/article/blockchain-basics-building-a-blockchain-in-net-core/

---
+++ blockchain_csharp_tutorial/BlockChainDemo/BlockChainDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace BlockchainDemo {
+    class Program {
+        static void Main(string[] args) {
+            //Create blockchain
+            Blockchain rourkesCoin = new Blockchain();
+            rourkesCoin.AddBlock(new Block(DateTime.Now, null, "{sender:MaHesh,receiver:Henry,amount:5}"));
+            rourkesCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Bob,receiver:Billy,amount:10}"));
+            rourkesCoin.AddBlock(new Block(DateTime.Now, null, "{sender:Dylon,receiver:Kael,amount:5}"));
+
+            //Output blockchain
+            Console.WriteLine(JsonConvert.SerializeObject(rourkesCoin, Formatting.Indented));
+
+            //Validate chain
+            Console.WriteLine($"Is Chain Valid: {rourkesCoin.IsValid()}");
+
+
+            //Tamper with chain
+            Console.WriteLine($"Update amount to 1000");
+            rourkesCoin.Chain[1].Data = "{sender:Henry,receiver:MaHesh,amount:1000}";
+            //Output tampered chain
+            //Console.WriteLine(JsonConvert.SerializeObject(rourkesCoin, Formatting.Indented));
+
+            //Validate bad chain
+            Console.WriteLine($"Is Chain Valid: {rourkesCoin.IsValid()}");
+        }
+    }
+}
